392db83e4ac35407fb1877aabd0acbd8acfe798e
Mohiuddin <52062238+Mohiuddin-diu@users.noreply.github.com>
2020-04-23 11:56:13 +0600

    Add files via upload

--- source code.c
+++ source code.c
@@ -44,7 +44,6 @@ struct books
 
 } book;
 FILE *BOOKS,*temp;
-
 struct members
 {
     char name[20];
@@ -1279,12 +1278,15 @@ int main()
     printf("Thank You (^-^)\n");
     char curr_time[10];
     char curr_date[12];
-
+    int std_c;
 
     strcpy(curr_time, __TIME__);
     strcpy(curr_date, __DATE__);
     printf("TIME : %s \nDATE : %s\n", curr_time, curr_date);
-    
+    printf("This is line %d\n", __LINE__);
+    std_c = __STDC__;
+    printf("STDC is %d", std_c);
+
 
     return 0;
 }
