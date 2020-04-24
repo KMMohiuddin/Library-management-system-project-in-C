4fbf086bedace99a62e90ad2b020cf862dcfd739
Mohiuddin <52062238+Mohiuddin-diu@users.noreply.github.com>
2020-04-24 15:00:50 +0600

    Update source code.c

--- source code.c
+++ source code.c
@@ -61,6 +61,30 @@ void gotoxy (int x, int y)
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+int main()
+{
+
+    system("COLOR F2");
+    Password();
+    system("cls");
+    gotoxy(1,1);
+    printf("Thank You (^-^)\n");
+    char curr_time[10];
+    char curr_date[12];
+    int std_c;
+
+    strcpy(curr_time, __TIME__);
+    strcpy(curr_date, __DATE__);
+    printf("TIME : %s \nDATE : %s\n", curr_time, curr_date);
+    printf("This is line %d\n", __LINE__);
+    std_c = __STDC__;
+    printf("STDC is %d", std_c);
+
+
+    return 0;
+}
+
 void firstbox(char text[40])
 {
     int i;
@@ -1268,25 +1292,3 @@ void editbooks()
 
 
 
-int main()
-{
-
-    system("COLOR F2");
-    Password();
-    system("cls");
-    gotoxy(1,1);
-    printf("Thank You (^-^)\n");
-    char curr_time[10];
-    char curr_date[12];
-    int std_c;
-
-    strcpy(curr_time, __TIME__);
-    strcpy(curr_date, __DATE__);
-    printf("TIME : %s \nDATE : %s\n", curr_time, curr_date);
-    printf("This is line %d\n", __LINE__);
-    std_c = __STDC__;
-    printf("STDC is %d", std_c);
-
-
-    return 0;
-}
